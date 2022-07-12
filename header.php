79d794005e2d4398ccc4577951e1247b0e39ea5c
Ben Avnilov <105242955+BenZiAv@users.noreply.github.com>
2022-07-12 21:10:55 +0300

    Create header.php
    header include navbar

---
+++ header.php
@@ -0,0 +1,15 @@
+<?php session start() ?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width", initial-scale=1.0">
+    <link href="./style.css" rel="stylesheet" type"text/css">
+    
+    <title> ResearchPro </title>
+  </head>
+  <div class="navbar">
+    <a herf="./researchers.php"><i>Researchers</i></a>
+    <a> herf="./research.php"><i>Research</i></a>
+  </div>
