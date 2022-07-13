7a57d6d23009c984158ea5829d195940e8524e68
BenZiAv <benavnilov@gmail.com>
2022-07-13 22:07:55 +0300

    header w

---
+++ pages/header.php
@@ -0,0 +1,19 @@
+<?php session_start() ?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width", initial-scale=1.0">
+    <link href="../style/style.css" rel="stylesheet" type="text/css" >
+    
+    <title> ResearchPro </title>
+  </head>
+  <div class="navbar">
+    <ul>
+        <a herf="./welcome.php" > Welcome </a>
+        <a herf="./researchers.php" > Researchers </a>
+        <a herf="./research.php" > Research </a>
+    </ul>
+  </div>
+  <br/> <br/> <br/> <br/> 
--- pages/research.php
+++ pages/research.php
@@ -1,18 +1,4 @@
-<?php session_start() ?>
-
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width", initial-scale=1.0">
-    <link href="../style.css" rel="stylesheet" type"text/css">
-    
-    <title> ResearchPro </title>
-  </head>
-  <div class="navbar">
-    <a herf="./researchers.php"><i>Researchers</i></a>
-    <a herf="./research.php"><i>Research</i></a>
-  </div>
+<?php include './header.php'; ?>
 
 <body>
   <h1> Welcome </h1><br>
