3bb58fa6c7c219f0e0973b221cff4886c28d33e6
BenZiAv <benavnilov@gmail.com>
2022-07-27 21:30:32 +0300

    db

--- catomProject.sql
+++ catomProject.sql
@@ -1,19 +1,19 @@
-CREATE DATABASE catomProject;
+CREATE DATABASE CatomProject;
 
 CREATE TABLE researchers(rID INT NOT NULL,
                          firstName VARCHAR(25) NOT NULL,
                          lastName VARCHAR(25) NOT NULL,
                          Summary VARCHAR(4000) NOT NULL,
+                         picture VARBINARY(255) NOT NULL,
                          
-                         pID INT NOT NULL,
-                         PRIMARY KEY (hID,pID),
-                         FOREIGN KEY (pID) REFERENCES projects (pID) ON DELETE CASCADE)
-
+                         PRIMARY KEY (rID));
+                         
 
 
 CREATE TABLE projects(pID INT NOT NULL,
-                      projectName VARCHAR(25) NOT NULL,
-                      subject VARCHAR(25) NOT NULL,
+                      projectTitle VARCHAR(25) NOT NULL,
+                      date DATE NOT NULL,
+                      Summary VARCHAR(4000) NOT NULL,
                       rID INT NOT NULL,
                       PRIMARY KEY (rID,pID),
-                      FOREIGN KEY (rID) REFERENCES researchers (rID) ON DELETE CASCADE)
+                      FOREIGN KEY (rID) REFERENCES researchers (rID) ON DELETE CASCADE);
